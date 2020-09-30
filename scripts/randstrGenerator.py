b4a0e867005dc1565fb4c26ac79247bb2f429271
RinHizakura <s921975628@gmail.com>
2020-10-01 00:52:59 +0800

    Generate new random file

--- scripts/randstrGenerator.py
+++ scripts/randstrGenerator.py
@@ -12,7 +12,7 @@ x, y = ct.shape
 
 opt = ['a', 'd']
 f = open('input_rand.txt', 'w')
-for i in range(0,500000):
+for i in range(0,1000):
      act = random.choice(opt)
      f.write(act)
      if(act == 'd'):
